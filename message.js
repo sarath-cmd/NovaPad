58e0520babdfa8a7b4eadd50f4ade3e713f0a609
sarath-cmd <131430271+sarath-cmd@users.noreply.github.com>
2023-12-17 14:13:15 +0530

    Add files via upload

---
+++ message.js
@@ -0,0 +1,6 @@
+let note="Please ensure that the mouse pointer is on the webpage so to load the transition of the contants";
+alert(note);
+
+let note1="After scrolling if the transition does not load automatically pls move the pointer slightly";
+alert(note1);
+
